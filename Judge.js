adeaa53b3eaf3edacaed5b6afea342a297c20a3a
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-08-28 10:05:09 +0300

    Stuff.

--- Judge.js
+++ Judge.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+
 const Judge = () => {
 
   const getVerdict = (evidence) => {
@@ -9,16 +11,24 @@ const Judge = () => {
       const commentKarma = Number(evidence.comment_karma)
       const postKarma = Number(evidence.link_karma)
       const postVsCommentDelta = postKarma - commentKarma
+      const accountCreated = moment(evidence.created, 'X').format('DD.MM.YYYY')
+      const accountAge = moment(evidence.created, 'X').fromNow()
 
       if (postVsCommentDelta < 0) {
         resolve({
-          verdict: 'User has more comment karma than post karma, so probably not as asshole.'
+          verdict: 'User has more comment karma than post karma, so probably not as asshole.',
+          accountAge,
+          accountCreated,
+          debugData: evidence,
         })
       }
 
       // We're not sure, return some values for possible further investigation.
       resolve({
-        postVsCommentDelta
+        postVsCommentDelta,
+        accountAge,
+        accountCreated,
+        debugData: evidence,
       })
     })
   }
