644e6a2ac96902b2fd209c6458b0f30e12e7cf9e
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-10-12 12:43:52 +0300

    Copy stuff

--- Police.js
+++ Police.js
@@ -6,10 +6,31 @@ const Police = () => {
   const patrol = async () => {
     const url = 'https://www.reddit.com/r/all.json'
     const response = await axios.get(url)
-    console.log(response.data)
-    return {
-      todo: true,
-    }
+    return getSuspiciousPosts(response.data.data.children)
+  }
+
+  const getSuspiciousPosts = (posts) => {
+    return posts.filter(post => {
+      const data = post.data
+
+      // Has to have lots of up votes.
+      if (data.ups < 10000) {
+        return false
+      }
+
+      // "Self" posts are rarely reposts.
+      if (data.is_self) {
+        return false
+      }
+
+      // "Pinned" and "Stickied" posts can't be reposts.
+      if (data.pinned || data.stickied) {
+        return false
+      }
+
+      // Guilty until proven innocent!
+      return true
+    })
   }
 
   const getUserData = async (username) => {
